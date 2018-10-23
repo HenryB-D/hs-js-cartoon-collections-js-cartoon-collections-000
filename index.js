49f507f0fa70c712658b0a8eed9227b7451d4592
Henry Brooke-Dean <henrybrookedean@hotmail.com>
2018-10-23 08:27:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 function dwarfRollCall(dwarves) {
+  var rollcall = ""
+  for (i = 0; i< dwarves.length; i++);
+  rollcall += (i+1 + "." + dwarves[i] + ",")
 }
+return rollcall
 
 function summonCaptainPlanet(planeteerCalls){
+  return planeteerCalls.map(a=>a.toUpperCase()+"!")
+ 
 }
 
 function longPlaneteerCalls(words) {
+  for(var i=0;i<words.length;i++){
+    if (words[i].length>4){
+      return true
+    }
+  }
+  return false
 }
 
 function findTheCheese (foods) {
+for(var i=0;i<foods.length;i++){
+  if ([i]=="cheddar", [i]=="gouda", [i]=="camembert"){
+    return [i]
+  }
+  }
+return "no cheese!"
 }
